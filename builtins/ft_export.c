3eb310d4d474c6f48246c07f70f6742880e9df5d
Danae Duvivier <Danae.Duvivier@ulb.ac.be>
2022-11-15 02:45:40 +0100

    UPDATE: export works with arguments. sort function remained

--- builtins/ft_export.c
+++ builtins/ft_export.c
@@ -24,6 +24,45 @@ int	ft_puterror(char *arg, char *str)
 	return (0);
 }
 
+/* Update the value of the environment variable found with key. */
+int	ft_update_env_value(char *key, char *new_value, t_env *env)
+{
+	t_env	*env_var;
+
+	env_var = env;
+	while (env_var != NULL)
+	{
+		if (ft_strncmp(key, env_var->key, ft_strlen(key) + 1) == 0)
+		{
+			free(env_var->value);
+			env_var->value = NULL;
+			env_var->value = new_value;
+			return (1);
+		}
+		env_var = env_var->next;
+	}
+	return (0);
+}
+
+char	*ft_get_new_env_val(char *str, char *key, int *add_flag, t_env *env)
+{
+	char	*value;
+	char	*return_value;
+
+	value = ft_cpy_env_val(str);
+	if (!value)
+		return (NULL);
+	if (*add_flag)
+	{
+		return_value = ft_strjoin(value, ft_getenv(key, env));
+		free(value);
+		value = NULL;
+	}
+	else
+		return_value = value;
+	return (return_value);
+}
+
 int	ft_builtin_export(char **args, t_env *env)
 {
 	//t_env	*new_env_var;
@@ -49,27 +88,20 @@ int	ft_builtin_export(char **args, t_env *env)
 		key = ft_cpy_env_key(*args, add_flag);
 		if (!key)
 			return (0);
+		value = ft_get_new_env_val(*args, key, &add_flag, env);
+		if (!value)
+		{
+			free(key);
+			return (0);
+		}
 		if (ft_env_var_exist(key, env))
 		{
-			printf("EXIST\n");
-			value = ft_getenv(key, env);
-			printf("%s\n", value);
-			// if addition mark is in str
-			// 		join 2 string
-			// else
-			// 		assign new value
+			ft_update_env_value(key, value, env);
 		}
 		else
 		{
-			printf("NOT EXIST\n");
-			// create new env_var
+			printf("NOT EXIST need to create one.\n");
 		}
-			//{
-			//	new_env_var = ft_new_env_var(*args);
-			//	if (!new_env_var)
-			//		return (0);
-			//	env = ft_add_env_var(env, new_env_var);
-			//}
 		free(key);
 		args++;
 	}
